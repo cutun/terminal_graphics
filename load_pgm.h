f77d1bd566575d0c2adcce8f5d18e60010ee7f86
MinhTran <thanthau@gmail.com>
2025-01-16 15:57:58 -0800

    rearrange the code

--- load_pgm.h
+++ load_pgm.h
@@ -1,60 +1,92 @@
-#ifndef __PGM_H__
-#define __PGM_H__
+#ifndef PGM_H
+#define PGM_H
 
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <exception>
+#include <stdexcept>
 #include <limits>
-
+#include <vector>
+#include <span>
+#include <concepts>
+#include <ranges>
+#include <format>
 #include "terminal_graphics.h"
-
 
 // Simple function to load an ascii-encoded PGM grayscale image.
 // This returns an object of type TG::Image<ValueType>
 
+template<typename T>
+concept PixelType = std::unsigned_integral<T> || std::floating_point<T>;
 
-template <typename ValueType = unsigned char>
-TG::Image<ValueType> load_pgm (const std::string& pgm_filename)
-{
-  std::ifstream in (pgm_filename);
-  if (!in)
-    throw std::runtime_error ("failed to open input PGM file \"" + pgm_filename + "\"");
+namespace detail {
+    [[nodiscard]] inline std::stringstream preprocess_pgm_file(const std::string_view filename) {
+        std::ifstream file{filename.data()};
+        if (!file) {
+            throw std::runtime_error(std::format("Failed to open PGM file: {}", filename));
+        }
 
-  // strip out all comments, and present stringstream of clean file contents:
-  std::string line;
-  std::stringstream in_clean;
-  while (std::getline (in, line)) {
-    in_clean << line.substr (0, line.find_first_of('#')) << "\n";
-  }
-
-  std::string magic;
-  in_clean >> magic;
-  if (magic != "P2")
-    throw std::runtime_error ("input file \"" + pgm_filename + "\" is not in expected PGM format");
-
-  int xdim, ydim, maxval;
-  in_clean >> xdim >> ydim >> maxval;
-
-  if (maxval >= 65536)
-    throw std::runtime_error ("PGM file \"" + pgm_filename + "\" is badly formed: maxval exceeds 65536");
-  if (maxval <= 0)
-    throw std::runtime_error ("PGM file \"" + pgm_filename + "\" is badly formed: maxval lower than or equal to zero");
-
-  if (maxval > std::numeric_limits<ValueType>::max())
-    throw std::runtime_error ("maximum intensity in PGM file \"" + pgm_filename + "\" exceeds range of data type used");
-
-  TG::Image<ValueType> im (xdim, ydim);
-
-  for (int y = 0; y < im.height(); ++y) {
-    for (int x = 0; x < im.width(); ++x) {
-      int val;
-      in_clean >> val;
-      im(x,y) = static_cast<ValueType> (val);
+        std::stringstream clean_content;
+        std::string line;
+        
+        while (std::getline(file, line)) {
+            if (const auto comment_pos = line.find('#'); comment_pos != std::string::npos) {
+                line.resize(comment_pos);
+            }
+            if (!line.empty()) {
+                clean_content << line << '\n';
+            }
+        }
+        return clean_content;
     }
-  }
 
-  return im;
+    inline void validate_pgm_header(std::string_view magic, int width, int height, 
+                                  int max_val, std::string_view filename) {
+        if (magic != "P2") {
+            throw std::runtime_error(std::format("Invalid PGM format in file: {}", filename));
+        }
+
+        if (max_val >= 65536 || max_val <= 0) {
+            throw std::runtime_error(std::format("Invalid maxVal {} in PGM file: {}", 
+                                               max_val, filename));
+        }
+
+        if (width <= 0 || height <= 0) {
+            throw std::runtime_error(std::format("Invalid dimensions {}x{} in PGM file: {}", 
+                                               width, height, filename));
+        }
+    }
 }
 
-#endif
+template <PixelType ValueType = unsigned char>
+[[nodiscard]] TG::Image<ValueType> load_pgm(const std::string_view filename) {
+    auto content = detail::preprocess_pgm_file(filename);
+
+    // Read header
+    std::string magic;
+    int width, height, max_val;
+    content >> magic >> width >> height >> max_val;
+
+    detail::validate_pgm_header(magic, width, height, max_val, filename);
+
+    if (max_val > std::numeric_limits<ValueType>::max()) {
+        throw std::runtime_error(
+            std::format("Maximum intensity {} exceeds data type range in file: {}", 
+                       max_val, filename)
+        );
+    }
+
+    TG::Image<ValueType> image(width, height);
+    
+    for (const auto y : std::views::iota(0, height)) {
+        for (const auto x : std::views::iota(0, width)) {
+            int val;
+            content >> val;
+            image(x, y) = static_cast<ValueType>(val);
+        }
+    }
+
+    return image;
+}
+
+#endif // PGM_H
